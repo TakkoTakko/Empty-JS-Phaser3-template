6c304069f306099975fdcf39680c544b081bf783
webbmen-mac <praktikant1@webmen.de>
2024-01-11 10:47:26 +0100

    disabled debug mode

--- main.js
+++ main.js
@@ -298,7 +298,7 @@ const config = {
         default: "arcade",
         arcade: {
             gravity: { y: speedDown },
-            debug: true,
+            debug: false,
         },
     },
     plugins: {},
