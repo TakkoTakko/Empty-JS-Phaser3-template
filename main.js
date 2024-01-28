a31b105eb35e966957e3e63cd396c8059429c7dd
Tjark Städtler <staedtler.tjark@gmail.com>
2024-01-28 20:12:00 +0100

    Revision

--- main.js
+++ main.js
@@ -3,39 +3,50 @@ import Phaser from 'phaser';
 
 class GameScene extends Phaser.Scene {
 
-
     constructor() {
+        // returns Phaser.Scene name
         super("scene-game");
-
     }
 
     preload() {
-        console.log('preload')
+        console.log('{Phaser3} {GameScene} preload');
+
+        // Here you load all your assets
+        // This is called once, at startup
     }
 
-
-
     create() {
-        console.log('create')
-        this.add.text(800, 700, 'Hello World', { fill: '#0f0' })
+        console.log('{Phaser3} {GameScene} create');
+
+        // Here you create all your objects
+        // This is called once
+
+        /**
+         * Hello world text
+         * @type {Phaser.GameObjects.Text}
+         */
+        this.add.text(600, 15, 'Hello World', { fill: '#0f0', fontSize: '120px' })
     }
 
     update() {
-        console.log('update');
+        console.log('{Phaser3} {GameScene} update');
+
+        // Here you update all your objects
+        // This is called every frame
     }
 }
 
-
-
-
-
+/**
+ * default game config
+ * @type {{backgroundColor: string, canvas: *, plugins: {}, physics: {default: string, arcade: {debug: boolean, gravity: {y: number}}}, width: number, type: number, height: number, scene: GameScene[]}}
+ */
 
 const config = {
-    type: Phaser.WEBGL,
-    width: 1920,
-    height: 1080,
+    type: Phaser.WEBGL, // select the WebGL or Canvas renderer, otherwise AUTO
+    width: 1920, // width of the game area
+    height: 1080, // height of the game area
     backgroundColor: '#333333',
-    canvas: gameCanvas,
+    canvas: gameCanvas, // This is the id of the canvas tag in your index.html file
     physics: {
         default: "arcade",
         arcade: {
@@ -43,7 +54,7 @@ const config = {
             debug: true,
         },
     },
-    plugins: {},
+    plugins: {}, // load phaser plugins here
     scene: [GameScene],
 };
 
---
+++ read.md
@@ -0,0 +1,25 @@
+# Empty Phaser 3 template with JavaScript
+
+
+
+## How to use
+- Clone this repository
+- Run `npm install`
+- Run `npm run dev`
+
+###
+
+## How to build
+- Run `npm run build`
+
+###
+
+## How to start editing
+- change default site structure in `index.html`
+- Change style in `style.css`
+- Change code in `main.js`
+
+
+###
+###
+Made with ♥️ by Tjark Städtler ©️ 2024
